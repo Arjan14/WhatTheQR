4053bf5e9e2649db89f6d03f368969d76f84b336
Arjan <55742327+Arjan14@users.noreply.github.com>
2022-01-09 15:10:56 +0100

    Can generate qr code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, send_file
+from flask_cors import CORS
+import qrcode
+from io import BytesIO
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -18,6 +22,17 @@ def make():
 def upload():
     return render_template("upload.html")
 
+@app.route('/generate_qrcode', methods=['POST'])
+def generate_qrcode():
+    buffer = BytesIO()
+    data = request.form.get('data')
+
+    img = qrcode.make(data)
+    img.save(buffer)
+    buffer.seek(0)
+
+    response = send_file(buffer, mimetype='image/png')
+    return response
 
 if __name__ == '__main__':
     app.run()
