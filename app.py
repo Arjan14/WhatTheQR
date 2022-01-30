c779f08ac088dc9fdff5bcb8f07126840485d02e
Arjan <55742327+Arjan14@users.noreply.github.com>
2022-01-30 20:12:58 +0100

    Kan fotos uploaden

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, redirect, url_for, render_template, request, send_file
-from flask_cors import CORS
-import qrcode
 from io import BytesIO
+import os
+import qrcode
+from flask import Flask, redirect, url_for, render_template, request, send_file, abort, send_from_directory
+from flask_cors import CORS
+import imghdr
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+def validate_image(stream):
+    header = stream.read(512)  # 512 bytes should be enough for a header check
+    stream.seek(0)  # reset stream pointer
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
 
 @app.route("/")
 def home():
@@ -18,9 +32,10 @@ def learn():
 def make():
     return render_template("make.html")
 
-@app.route("/upload")
-def upload():
-    return render_template("upload.html")
+@app.route("/uploadfile")
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template("upload.html", files=files)
 
 @app.route('/generate_qrcode', methods=['POST'])
 def generate_qrcode():
@@ -34,7 +49,22 @@ def generate_qrcode():
     response = send_file(buffer, mimetype='image/png')
     return response
 
+@app.route('/uploadfile', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
 
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 if __name__ == '__main__':
     app.run()
